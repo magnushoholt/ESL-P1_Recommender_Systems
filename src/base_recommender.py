1eddd23feab6d1603f34e50b1c80ef0bb857433c
Andersx2 <Anders.o.l@outlook.dk>
2026-03-11 10:45:08 +0100

    expanded the base of recommender

--- src/base_recommender.py
+++ src/base_recommender.py
@@ -2,8 +2,16 @@ from abc import ABC, abstractmethod
 
 
 class BaseRecommender:
+    def __init__(self, name):
+        self.name = name
+
     @abstractmethod
-    def fit(self, train_data):
+    def fit(self, train_data, item_df):
+        """Fit the model to the training data"""
+        pass
+
+    def predict_rating(self, user_id, item_id):
+        """Returns a predicted rating (1-5) for accuracy evaluation"""
         pass
 
     @abstractmethod
@@ -11,6 +19,8 @@ class BaseRecommender:
         """
         Should return a list of (item_id, score, explanation).
         This is millers way of adressing "cause of a decision"
+        Example:
+        - Returns a list of dictionaries:
+        - [{'item_id': 101, 'score': 4.5, 'explanation': 'Because you liked Byggemand Bob'}]
         """
         pass
-    
