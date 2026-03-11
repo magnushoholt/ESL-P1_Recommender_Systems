026f8ee8d57a9e8e356c85d5c7e3f3cb69b26774
magnushoholt <magnushoho@gmail.com>
2026-03-11 09:55:15 +0100

    Update data_prep.py

--- src/data_prep.py
+++ src/data_prep.py
@@ -13,7 +13,7 @@ def matrix_data():
 
 
 def load_item():
-    df = pd.read_csv('data/u.item', sep='|',header=None)
+    df = pd.read_csv('data/u.item', sep='|', header=None, encoding='latin-1')
     df.columns = ["movie_id", "movie_title", "release_date", "video_release_date",
               "IMDb_URL", "unknown", "Action", "Adventure", "Animation",
               "Children", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
@@ -25,15 +25,58 @@ def load_info():
     df = pd.read_csv('data/u.info', sep=r' ', header=None)
     return df
 
+def cleaning_data(items_df, ratings_df):
+    # Find duplicates
+    dup = items_df.groupby("movie_title")["movie_id"].apply(list)
+    duplicates = dup[dup.str.len() > 1]
+    
+    # Mapping
+    mapping = {ids[1]: ids[0] for ids in duplicates}
+    ids_to_change = list(mapping.keys())
+
+    # For testing if everything is correct after
+    untouched_mask = ~ratings_df["item_id"].isin(ids_to_change)
+    
+    # Saving data not to change
+    expected_untouched_data = ratings_df[untouched_mask].copy()
+
+    # Change values
+    ratings_df["item_id"] = ratings_df["item_id"].replace(mapping)
+
+    # Check if all is changed (should be 0)
+    remaining_duplicates = ratings_df["item_id"].isin(ids_to_change).sum()
+    
+    # Check if all other data is intact
+    current_untouched_data = ratings_df[untouched_mask]
+    integrity_check = expected_untouched_data.equals(current_untouched_data)
+    
+    # Print Results
+    if not integrity_check:
+        print(f"1. Duplicate IDs remaining: {remaining_duplicates} (Expected: 0)")
+        print(f"2. Data Integrity Check: FAILED")
+
+    # Clean data and preserve order
+    rating_column_order = ratings_df.columns.tolist()
+    ratings_clean = ratings_df.groupby(["user_id","item_id"], as_index=False).mean() #Fucker tiden up men virker for alt andet
+    ratings_clean = ratings_clean.reindex(columns=rating_column_order)
+
+    item_column_order = items_df.columns.tolist()
+    items_clean = items_df.groupby(["movie_title"], as_index=False).min()
+    items_clean = items_clean.reindex(columns=item_column_order)
+
+    return items_clean, ratings_clean
 
 
 # Example usage
 if __name__ == "__main__":
     ratings_df = load_data()
     items_df = load_item()
+    print(f"Number of movies with multiple id's: {items_df["movie_title"].value_counts().value_counts()}")
+    item_clean, ratings_clean = cleaning_data(items_df,ratings_df)
+    print(ratings_clean.head())
+    print(f"Number of ratings after cleaning: {ratings_clean.shape[0]}")
+    print(item_clean.head())
     info_df = load_info()
-    print(ratings_df.head())
-    print(items_df.head())
     print(info_df.head())
     matrix_df = matrix_data()
     print(matrix_df.head())
